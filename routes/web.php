116b83789a7271f0e53697d877cfe4c886bc4657
karasekadam <48647780+karasekadam@users.noreply.github.com>
2021-03-18 00:16:42 +0100

    full paths

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MatchController;
+use App\Http\Controllers\mainControl;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,26 +15,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/',"MatchController@home")->name("home");
+// pls nech tam ty celý cesty, bez nich mi to nefunguje
 
-Route::get('/login', "MatchController@login")->name("login");
+Route::get('/',"App\Http\Controllers\MatchController@home")->name("home");
 
-Route::get('/blog', "MatchController@blog")->name("blog");
+Route::get('/login', "App\Http\Controllers\MatchController@login")->name("login");
 
-Route::get('/aboutus', "MatchController@aboutus");
+Route::get('/blog', "App\Http\Controllers\MatchController@blog")->name("blog");
 
-Route::get('/admin_blog', "MatchController@adm_blog")->name("adm_blog");
+Route::get('/aboutus', "App\Http\Controllers\MatchController@aboutus");
 
-Route::get('/admin_matches', "MatchController@adm_matches")->name("adm_matches");
+Route::get('/admin_blog', "App\Http\Controllers\MatchController@adm_blog")->name("adm_blog");
+
+Route::get('/admin_matches', "App\Http\Controllers\MatchController@adm_matches")->name("adm_matches");
 
 //Route::get('ajax-autocomplete-search', "App\Http\Controllers\MatchController@search");
 
-Route::post("/log_check", 'mainControl@check_login');
+Route::post("/log_check", 'App\Http\Controllers\mainControl@check_login');
 
-Route::post("/reg_check", 'mainControl@check_reg');
+Route::post("/reg_check", 'App\Http\Controllers\mainControl@check_reg');
 
-Route::post("/update_values", 'mainControl@update_other');
+Route::post("/update_values", 'App\Http\Controllers\mainControl@update_other');
 
-Route::post("/update_match", 'mainControl@update_match');
+Route::post("/update_match", 'App\Http\Controllers\mainControl@update_match');
 
 //Route::get('/{sport}/{league}',"App\Http\Controllers\MatchController@league");
