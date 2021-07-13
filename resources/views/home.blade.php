d5c908cb3dd55702e850572309cb7ecbcf15d97c
Tars016 <dan.grbin@seznam.cz>
2021-07-13 14:07:23 +0200

    new design

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,22 +23,22 @@
     </script>
 
         <div id="wrap" style="height: 90vh; overflow-x: hidden; overflow-y: auto">
-            <table class="table">
-                <thead class="thead-light">
+            <table class="table" style="background-color: #f7f7f7">
+                <thead>
                     <tr>
-                        <th class="date align-middle d-none d-md-table-cell font-weight-light h4"
-                        style="width: 7%">Date</th>
-                        <th class="match align-middle p-2 pl-2 p-md-3 font-weight-light h4" style="width: 31%">Match</th>
-                        <th class="league align-middle d-none d-sm-table-cell font-weight-light h4" style="width: 5%">Leag.</th>
-                        <th class="rate align-middle text-center p-2 p-md-3 font-weight-light h4" style="width: 19%">1</th>
-                        <th class="rate align-middle text-center p-2 p-md-3 font-weight-light h4" style="width: 19%">X</th>
-                        <th class="rate align-middle text-center p-2 p-md-3 font-weight-light h4" style="width: 19%">2</th>
+                        <th class="match d-none d-md-table-cell font-weight-light h5 pb-1"
+                        style="width: 43%"><span class="ml-2">Match</span></th>
+                        <!--<th class="match align-middle p-2 pl-2 p-md-3 font-weight-light h4" style="width: 31%">Match</th>
+                        <th class="league align-middle d-none d-sm-table-cell font-weight-light h4" style="width: 5%">Leag.</th>-->
+                        <th class="rate text-center pb-1 font-weight-light h5" style="width: 19%">1</th>
+                        <th class="rate text-center pb-1 font-weight-light h5" style="width: 19%">X</th>
+                        <th class="rate text-center pb-1 font-weight-light h5" style="width: 19%">2</th>
                     </tr>
                 </thead>
                 <tbody>
                 @foreach($matches as $match)
                     <tr>
-                        <td class="date d-none d-md-table-cell align-middle pl-4"><span class="match_date">{{$match->date}}</span><br>11:11</td>
+                        
                         @php
                         {{
                             $logo_title1 = str_replace("/", "-", $match->team1);
@@ -46,28 +46,32 @@
                         }}
                         @endphp
                         @if (is_file('img/logo/' . $logo_title1 . '.png'))
-                            <td class="match d-none d-md-table-cell align-middle pt-1 pb-1"><img src="img/logo/{{$logo_title1}}.png" class="web_logo">{{$match->team1}} -
+                            <td class="date d-none d-md-table-cell align-middle pl-4"><img src="img/logo/{{$logo_title1}}.png" class="web_logo">{{$match->team1}}<br>
                         @else
-                            <td class="match d-none d-md-table-cell align-middle pt-1 pb-1">{{$match->team1}} -
+                            <td class="date d-none d-md-table-cell align-middle pl-4">{{$match->team1}}<br>
                         @endif
 
                         @if (is_file('img/logo/' . $logo_title2 . '.png'))
-                            <img src="img/logo/{{$logo_title2}}.png" class="web_logo">{{$match->team2}}</td>
+                            <img src="img/logo/{{$logo_title2}}.png" class="web_logo">{{$match->team2}}<br>
                         @else
-                            {{$match->team2}}</td>
+                            {{$match->team2}}<br>
                         @endif
-
+                        <span class="match_date pl-1" style="font-size: 85%">{{$match->date}}</span><span style="font-size: 85%">, 11:11</span> 
+                            @if (is_file('img/logo/' . $match->league . '.png'))
+                            <img src="img/logo/{{$match->league}}.png" style="width: 15px; height: 15px">
+                            @endif
+                        </td>
                         <td class="match d-md-none pt-1 pb-1"><img src="img/logo.png" class="mob_logo">{{$match->team1}}<br><img src="img/logo.png" class="mob_logo">{{$match->team2}}<br><span class="match_date">{{$match->date}}</span>, 11:11</td>
 
-                        <td class="league d-none d-md-table-cell align-middle">
+                        <!--<td class="league d-none d-md-table-cell align-middle">
                             @if (is_file('img/logo/' . $match->league . '.png'))
                             <img src="img/logo/{{$match->league}}.png" class="web_logo ml-3">
                             @endif
-                        </td>
+                        </td>-->
 
-                        <td class="rate text-center align-md-middle"><div class="rate_pad"><b>{{$match->prob1}}<br>1.11</b></div></td>
-                        <td class="rate text-center align-md-middle"><div class="rate_pad"><b>{{$match->probtie}}<br>1.11</b></div></td>
-                        <td class="rate text-center align-md-middle"><div class="rate_pad"><b>{{$match->prob2}}<br>1.11</b></div></td>
+                        <td class="rate text-center align-md-middle"><div class="rate_pad font-weight-light"><b>{{$match->prob1}}<br>1.11</b></div></td>
+                        <td class="rate text-center align-md-middle"><div class="rate_pad font-weight-light"><b>{{$match->probtie}}<br>1.11</b></div></td>
+                        <td class="rate text-center align-md-middle"><div class="rate_pad font-weight-light"><b>{{$match->prob2}}<br>1.11</b></div></td>
                     </tr>
                 @endforeach
                 </tbody>
--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -19,54 +19,7 @@
         <link rel="stylesheet" href="css/pickdroid.css">
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <style>
-            /*.dropbtn {
-                
-                background-color: #424242;
-                color: white;
-                width: 124px;
-                height: 24px;
-                border: none;
-                cursor: pointer;
-            }
-            
-            .dropbtn:hover, .dropbtn:focus {
-                background-color: #2980B9;
-                outline: none;
-            }
-            .dropdown {
-                position: relative;
-                display: inline-block;
-            }*/
-            .dropdown-item:focus {
-                background-color: #f1f1f1;
-                color: black;
-            }
-
-            /*.dropdown-content {
-                display: none;
-                position: absolute;
-                background-color: #f1f1f1;
-                min-width: 160px;
-                overflow: auto;
-                box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-                z-index: 1000;
-            }
-
-            .dropdown-content button {
-                color: black;
-                padding: 12px 16px;
-                text-decoration: none;
-                display: block;
-                width: 100%;
-            }*/
-
-            .dropdown a:hover {background-color: #ddd;}
-
-            .show {display: block;}
-        </style>
     </head>
-
     <body>
         <script>
             // funkce filtru
@@ -221,6 +174,7 @@
 
         <div class="container-fluid">
             <div class="row" id="topbar_row">
+                <!--
                 <div class="col-md-2 col-12 sidebar text-md-center">
                     <div class="d-none d-sm-none d-md-block">
                             <a href="/">
@@ -243,13 +197,18 @@
                             <a href="/login"><i class="fa fa-sign-in fa-3x float-right" style="color: #FF8000"></i></a>
                         </div>
                     </div>
-                </div>
+                </div>-->
 
-                <div class="col-md-10 d-none d-sm-none d-md-block topbar">
+                <div class="col-md-12 d-none d-sm-none d-md-block topbar">
                     <div class="row h-100 align-items-center">
-                        <div class="col-xl-3 col-lg-2 col-md-1">
+                        <div class="col-xl-2">
+                            <a href="/">
+                                <img src="img/robot2.png" alt="logo" id="robot">
+                            </a>
+                        </div>
+                        <div class="col-xl-2 col-lg-2 col-md-1">
                             <div class="dropdown">
-                                <button onclick="" class="btn btn-dark dropdown-toggle" id="drpdwn" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Filter</button>
+                                <div onclick="" class="dropdown-toggle" id="drpdwn" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color: white; cursor: pointer;">Filter</div>
                                 <div class="dropdown-menu dropdown-content" aria-labelledby="drpdwn">
                                     <button class="dropdown-item" onclick="content_filter('2')">2 hours</button>
                                     <button class="dropdown-item" onclick="content_filter('4')">4 hours</button>
@@ -258,12 +217,12 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="col-xl-5 col-lg-6 col-md-4">
+                        <div class="col-xl-4 col-lg-6 col-md-4">
                             
                             <form class="form-inline" action="/action_page.php">
                                 <div class="form-group">
                                     <input type="text" class="form-control" placeholder="Search matches.." id="search" name="search" size="40">
-                                    <button type="button" onclick="search_site()" class="btn btn-dark" style="margin-left: 0.5em"><i class="fa fa-search"></i></button>
+                                    <div onclick="search_site()" class="" style="margin-left: 1em; color: white; cursor: pointer;"><i class="fa fa-search"></i></div>
                                 </div>
 
                             </form>
@@ -271,12 +230,12 @@
 
                         <div class="col-xl-4 col-lg-4 col-md-7">
                             @if ($user == "")
-                            <a href="/login"><button type="button" class="btn btn-dark float-right nav-btn">Login</button></a>
+                            <a href="/login"><div class="float-right mt-1 mr-3" style="color: white">Login</div></a>
                             @else
                             <div class="dropdown">
-                              <button class="btn btn-dark dropdown-toggle float-right nav-btn" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                              <div class="dropdown-toggle float-right" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 Účet
-                              </button>
+                              </div>
 
                               <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
                                 <a class="dropdown-item" href="account">Přehled účtu</a>
@@ -286,14 +245,15 @@
                               </div>
                             </div>
                             @endif
-                            <a href="/aboutus"><button type="button" class="btn btn-dark float-right nav-btn">About us</button></a>
-                            <a href="/blog"><button type="button" class="btn btn-dark float-right nav-btn">Blog</button></a>
+                            <a href="/aboutus"><div class="float-right mt-1 mr-4 pr-3 pl-3 font-weight-light" style="color: black; background-color: #ffdf1b; border-radius: 2px">Register</div></a>
+                            <!--
+                            <a href="/blog"><button type="button" class="btn btn-dark float-right nav-btn">Blog</button></a>-->
                         </div>
                     </div>
                 </div>
             </div>
 
-            <div class="row" style="height: 90vh">
+            <div class="row" style="height: 92vh">
                 <div class="col-md-2 d-md-block sidebar overflow-auto" id="menu" style="padding: 0px">
                     <div id="leagues" class="mt-md-4">
                         <form class="form-inline d-md-none">
@@ -302,17 +262,17 @@
                                 <input type="text" class="form-control" placeholder="Search matches.." id="search_mob" name="search">
                                 </div>
                                 <div class="col-4 pl-1">
-                                <button type="button" class="btn" style="background-color: #FF8000; color: #424242"><i class="fa fa-search"></i></button>
+                                <div class="" style="color: white"><i class="fa fa-search"></i></div>
                                 </div>
                             </div>
                         </form>
 
-                        <p class="h3 font-weight-light text-md-left" style="color: #FF8000; margin-bottom: 2%; margin-left: 2%">Top leagues</p>
+                        <p class="h6 text-md-left ml-4" style="color: #00378a; margin-bottom: 2%"><b>Top leagues</b></p>
                         @foreach($top_leagues as $league)
                         <div class="sidefont">
                             @if (is_null($league->our_name))
                             <a href="/?league={{$league->name_538}}">
-                                <p class="font-weight-light pt-2 pb-2 mt-0 mb-0 ml-2">
+                                <p class="font-weight-light pt-2 pb-2 mt-0 mb-0 ml-4">
                                     {{$league->name_538}}
                                 </p>
                             </a>
@@ -330,18 +290,18 @@
                         </div>
                         @endforeach
                     </div>
-                    <div style="border-bottom: 1px solid white; width: 80%; margin-left: 2%"></div>
-                    <div id="countries">
+                    <div id="countries" class="mt-3">
+                        <p class="h6 text-md-left ml-4" style="color: #00378a"><b>All matches</b></p>
                         @foreach($countries as $country)
                         <div class="sidefont">
-                            <p onclick="" class="font-weight-light pt-2 pb-2 mt-0 mb-0 ml-2 doToggle" id="{{$country->country}}">{{$country->country}}</p>
+                            <p class="pt-2 pb-2 mt-0 mb-1 ml-4 font-weight-light doToggle" id="{{$country->country}}">{{$country->country}}</p>
                         </div>
                             @foreach($leagues as $league)
                                @if ($league->country == $country->country)
-                                <div class="sidefont">
+                                <div>
                                     @if (is_null($league->our_name))
-                                    <a href="/?league={{$league->name_538}}">
-                                        <p class="sub_{{$country->country}} ml-3 mt-1 pt-1 pb-1" style="display: none; color: white">
+                                    <a href="/?league={{$league->name_538}}" style="text-decoration: none">
+                                        <p class="sub_{{$country->country}} ml-5 font-weight-light" style="display: none; color: black">
                                             {{$league->name_538}}
                                         </p>
                                     </a>
@@ -366,7 +326,7 @@
                     </div>
                 </div>
 
-                <div class="col-md-10 col-12 main" style="padding: 0px">
+                <div class="col-md-10 col-12 main" style="padding: 0px; background-color: #f7f7f7">
                     @yield("content")
                 </div>
             </div>
